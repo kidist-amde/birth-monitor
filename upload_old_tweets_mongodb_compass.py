0b7f4f526fdae48b81d659bfed1c533592c814b7
Luisa <82140971+luisalopresti@users.noreply.github.com>
2021-07-13 01:56:53 +0200

    Update upload_old_tweets_mongodb_compass.py

--- upload_old_tweets_mongodb_compass.py
+++ upload_old_tweets_mongodb_compass.py
@@ -4,6 +4,7 @@ import pymongo
 from pymongo import MongoClient
 from os import listdir
 from os.path import isfile, join
+import pathlib
 
 '''
 Script for converting csv files with old tweets by country into json files and uploading them into MongoDB Compass (locally).
@@ -19,16 +20,16 @@ db = myclient["TweetsDB"]
 
 # Create collections later
 
-# Loading and uploading the json files
-import pathlib
-path_tweets = pathlib.Path().resolve() # get current path
+# get current path
+path_tweets = pathlib.Path().resolve() 
 
-
+# path of tweets by country
 old_twees_path = os.path.join(path_tweets, "old_tweets")
 
 all_files = [f for f in listdir(old_twees_path) if isfile(join(old_twees_path, f)) and f.endswith(".csv")]
 
 
+# function to convert csv into json
 def convert_csv_to_json(file_path, file_name):
     import pandas as pd
     mydata = pd.read_csv(file_path)
@@ -45,7 +46,7 @@ def convert_csv_to_json(file_path, file_name):
     return tweets
 
 
-
+# upload into MongoDB
 for file_name in all_files:
     abs_file_path = os.path.join(old_twees_path, file_name)
     cntr_name = str(file_name.split("-")[0])
