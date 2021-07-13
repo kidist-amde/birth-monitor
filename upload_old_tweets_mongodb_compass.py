db5570ce982d08731058ea75905b9d729b949066
Luisa <82140971+luisalopresti@users.noreply.github.com>
2021-07-13 03:26:37 +0200

    Update upload_old_tweets_mongodb_compass.py

--- upload_old_tweets_mongodb_compass.py
+++ upload_old_tweets_mongodb_compass.py
@@ -46,7 +46,8 @@ def convert_csv_to_json(file_path, file_name):
     return tweets
 
 
-# upload into MongoDB
+'''
+# upload into MongoDB (each country into a different collection)
 for file_name in all_files:
     abs_file_path = os.path.join(old_twees_path, file_name)
     cntr_name = str(file_name.split("-")[0])
@@ -55,3 +56,13 @@ for file_name in all_files:
         file_data = csv.reader(f, delimiter=',')
         file_json = convert_csv_to_json(abs_file_path, file_name)
         collection.insert_many(file_json)
+'''
+
+# upload all tweets into a single collection (documents still have the country associated to the "location" key)
+single_collection = db["all_tweets"]
+for file_name in all_files:
+    abs_file_path = os.path.join(old_twees_path, file_name)
+    with open(abs_file_path, 'r', encoding='utf-8') as f:
+        file_data = csv.reader(f, delimiter=',')
+        file_json = convert_csv_to_json(abs_file_path, file_name)
+        single_collection.insert_many(file_json)
