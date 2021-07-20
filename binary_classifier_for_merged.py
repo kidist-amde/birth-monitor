6d05e86d3515762c73569bfdf607aaf61178a66a
Luisa <82140971+luisalopresti@users.noreply.github.com>
2021-07-20 23:49:14 +0200

    Update binary_classifier_for_merged.py

--- binary_classifier_for_merged.py
+++ binary_classifier_for_merged.py
@@ -26,7 +26,7 @@ Binary classifier of tweets (0 - false/not related, 1 - true/related).
 Each of the documents has _id, username, text, location, ID, time and a label. 
 The label (true/false) was put manually on this subset of documents, which will be used to train a classifier.
 
-This files just train the model and computes accuracy; 
+This files just train the model and computes AUC; 
 it does not apply the prediction on unlabeled tweets yet and does not output a json file as the previous version.
 '''
 
